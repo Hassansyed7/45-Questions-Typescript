e48453e7d5553af0a1aa5254819ea52025937343
Syed Hassan Bukhari <162698882+Hassansyed7@users.noreply.github.com>
2024-03-08 17:00:02 +0500

    Add files via upload

---
+++ Q5.ts
@@ -0,0 +1,6 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string="Hazrat Ali A.S said,"
+let message:string="Give your enemy a thousand changes to become your friend, but do not give your friend a single chance to become your enemy."
+
+console.log(`${famous_person} ${message}`)
