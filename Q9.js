beb9a6b96cacc56e09c92f79214e1f6205b8666f
Syed Hassan Bukhari <162698882+Hassansyed7@users.noreply.github.com>
2024-03-08 17:13:25 +0500

    Q9 of ts/js of 45 assignments
    Q9 of ts/js of 45 assignments

---
+++ Q9.js
@@ -0,0 +1,3 @@
+// 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var num = 2255;
+console.log("My Favourite number ".concat(2255));
---
+++ Q9.ts
@@ -0,0 +1,5 @@
+// 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let num:number=2255
+
+console.log(`My Favourite number ${2255}`)
