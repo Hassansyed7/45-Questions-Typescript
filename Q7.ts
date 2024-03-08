67dd6be20836e56e0d8989185e7ddadc233951a4
Syed Hassan Bukhari <162698882+Hassansyed7@users.noreply.github.com>
2024-03-08 17:11:08 +0500

    Q7 of ts/js of 45 assignments
    Q7 of ts/js of 45 assignments

---
+++ Q7.ts
@@ -0,0 +1,6 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+console.log(8+9);
+console.log(26-13);
+console.log(25*25);
+console.log(44100/10);
