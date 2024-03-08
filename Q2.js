4cfeb43b42f3da182d922167017971fe89e0f0d8
Syed Hassan Bukhari <162698882+Hassansyed7@users.noreply.github.com>
2024-03-08 16:49:06 +0500

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,3 @@
+// 2. Personal message: store a personal message in a variable , and print a message to that person . Your message should be simple, such as,"Hello Eric, would you like to learn some python today?"
+var firstName = "Eric";
+console.log("Hello ".concat(firstName, ", would you like to learn some python today?"));
---
+++ Q2.ts
@@ -0,0 +1,5 @@
+// 2. Personal message: store a personal message in a variable , and print a message to that person . Your message should be simple, such as,"Hello Eric, would you like to learn some python today?"
+
+let firstName:string= "Eric";
+
+console.log(`Hello ${firstName}, would you like to learn some python today?`);
