b4824c59bd662badbd2d7aaac3bb51b7f4a73b57
Syed Hassan Bukhari <162698882+Hassansyed7@users.noreply.github.com>
2024-03-11 13:29:53 +0500

    Q14.ts/js/tsconfig
    Q14.ts/js/tsconfig

---
+++ Q14.js
@@ -0,0 +1,3 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+var guestArr = ["Faiqa", "Raima", "Taqaddus", "Kainat", "Mehak"];
+guestArr.map(function (item) { return (console.log("".concat(item, ", \"you are invited to dinner tonight\""))); });
